0398d9df72910e43bd33dc10c477f1f930530daa
Thomas LEVEIL <thomasleveil@gmail.com>
2016-05-28 16:29:44 +0200

    more tests

---
+++ src/test/java/test/SomeTest2.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.Test;
+
+public class SomeTest2 extends Base {
+
+    @Test public void test1() {
+        run();
+    }
+
+    @Test public void test2() {
+        run();
+    }
+
+    @Test public void test3() {
+        run();
+    }
+
+    @Test public void test4() {
+        run();
+    }
+
+    @Test public void test5() {
+        run();
+    }
+
+}
